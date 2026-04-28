069e709b8d34d4fab3e16ff8fd454340c7f3a0ba
codagenz <codagenz10@gmail.com>
2026-04-28 22:13:59 +0300

    seeder removed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,41 +17,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(AcademicStructureSeeder::class);
+        // SEEDERS DISABLED - All seeding logic commented out
+        // To re-enable, uncomment the lines below:
 
-        $users = [
-            [
-                'name' => 'Instructor User',
-                'initials' => 'IU',
-                'email' => 'instructor@udom.com',
-                'role' => 'instructor',
-            ],
-            [
-                'name' => 'Admin User',
-                'initials' => 'AU',
-                'email' => 'admin@udom.com',
-                'role' => 'admin',
-            ],
-            [
-                'name' => 'Student User',
-                'initials' => 'SU',
-                'email' => 'student@udom.com',
-                'role' => 'student',
-            ],
-        ];
+        // $this->call(AcademicStructureSeeder::class);
 
-        foreach ($users as $userData) {
-            $user = User::query()->firstOrNew(['email' => $userData['email']]);
+        // $users = [
+        //     [
+        //         'name' => 'Instructor User',
+        //         'initials' => 'IU',
+        //         'email' => 'instructor@udom.com',
+        //         'role' => 'instructor',
+        //     ],
+        //     [
+        //         'name' => 'Admin User',
+        //         'initials' => 'AU',
+        //         'email' => 'admin@udom.com',
+        //         'role' => 'admin',
+        //     ],
+        //     [
+        //         'name' => 'Student User',
+        //         'initials' => 'SU',
+        //         'email' => 'student@udom.com',
+        //         'role' => 'student',
+        //     ],
+        // ];
 
-            $user->forceFill([
-                'name' => $userData['name'],
-                'initials' => $userData['initials'],
-                'email' => $userData['email'],
-                'role' => $userData['role'],
-                'email_verified_at' => now(),
-                'password' => Hash::make('@apes99U'),
-                'remember_token' => Str::random(10),
-            ])->save();
-        }
+        // foreach ($users as $userData) {
+        //     $user = User::query()->firstOrNew(['email' => $userData['email']]);
+
+        //     $user->forceFill([
+        //         'name' => $userData['name'],
+        //         'initials' => $userData['initials'],
+        //         'email' => $userData['email'],
+        //         'role' => $userData['role'],
+        //         'email_verified_at' => now(),
+        //         'password' => Hash::make('@apes99U'),
+        //         'remember_token' => Str::random(10),
+        //     ])->save();
+        // }
     }
 }
